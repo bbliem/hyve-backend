8668bfb87dd149ce62df73ccf6a029f02e6d5494
Bernhard Bliem <bernhard@bliem.xyz>
2020-11-19 01:30:55 +0200

    Updated project name

--- deploy-gitlab.sh
+++ deploy-gitlab.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 GITLAB_TEAM=gruifor-team
-GITLAB_PROJECT=hyventaminen-backend
+GITLAB_PROJECT=hyve-backend
 
 docker login registry.gitlab.com
 docker build -t registry.gitlab.com/${GITLAB_TEAM}/${GITLAB_PROJECT} .
