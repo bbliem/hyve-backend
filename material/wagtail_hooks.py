ce3bb79fbfc12dc1abd855bc2895edd727963049
Bernhard Bliem <bernhard@bliem.xyz>
2021-01-31 19:48:46 +0200

    Remove unused functionality from main menu

--- material/wagtail_hooks.py
+++ material/wagtail_hooks.py
@@ -1,5 +1,22 @@
 from wagtail.core import hooks
 
+
+@hooks.register('construct_main_menu')
+def hide_reports_menu_item(request, menu_items):
+    menu_items[:] = [m for m in menu_items if m.name != 'reports']
+
+
+@hooks.register('construct_settings_menu')
+def hide_settings_submenu_items(request, menu_items):
+    HIDDEN_ITEMS = [
+        'redirects',
+        'workflows',
+        'workflow-tasks',
+        'groups',
+    ]
+    menu_items[:] = [m for m in menu_items if m.name not in HIDDEN_ITEMS]
+
+
 @hooks.register('construct_page_listing_buttons')
 def hide_view_live_button(buttons, page, page_perms, is_parent=False, context=None):
     buttons[:] = [b for b in buttons if b.label != "View live"]
