a39aee2f06165361900291125f58843fa14d7785
Bernhard Bliem <bernhard@bliem.xyz>
2021-01-31 20:05:20 +0200

    Hide some unused menu items

--- material/wagtail_hooks.py
+++ material/wagtail_hooks.py
@@ -1,13 +1,22 @@
 from wagtail.core import hooks
 
 
+@hooks.register('construct_page_action_menu')
+def hide_page_action_menu_items(menu_items, request, context):
+    HIDDEN_ITEMS = [
+        'action-submit',  # "Submit to moderator's approval"
+        'action-lock',
+    ]
+    menu_items[:] = [m for m in menu_items if m.name not in HIDDEN_ITEMS]
+
+
 @hooks.register('construct_main_menu')
-def hide_reports_menu_item(request, menu_items):
+def hide_main_menu_items(request, menu_items):
     menu_items[:] = [m for m in menu_items if m.name != 'reports']
 
 
 @hooks.register('construct_settings_menu')
-def hide_settings_submenu_items(request, menu_items):
+def hide_settings_menu_items(request, menu_items):
     HIDDEN_ITEMS = [
         'redirects',
         'workflows',
@@ -18,5 +27,5 @@ def hide_settings_submenu_items(request, menu_items):
 
 
 @hooks.register('construct_page_listing_buttons')
-def hide_view_live_button(buttons, page, page_perms, is_parent=False, context=None):
+def hide_page_listing_buttons(buttons, page, page_perms, is_parent=False, context=None):
     buttons[:] = [b for b in buttons if b.label != "View live"]
